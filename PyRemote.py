21012e66abcef33229f4f684fb22b2d33f7ab30b
samuelluohaoen <samuelluohaoen@hotmail.com>
2021-01-13 00:49:50 -0800

    Need to fetch vision data for ball tracking

--- PyRemote.py
+++ PyRemote.py
@@ -11,7 +11,7 @@ RemoteCMD_Port = 6001
 cmd = RemoteAPI_pb2.Commands()
 cmd.enable_ball_auto_capture = False
 cmd.mode = 3
-cmd.is_world_frame = True
+cmd.is_world_frame = False
 cmd.motion_set_point.x, cmd.motion_set_point.y, cmd.motion_set_point.z = 0.0, 0.0, 0.0
 cmd.kicker_set_point.x, cmd.kicker_set_point.y = 0, 0
 
@@ -19,12 +19,16 @@ cmd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 pwr = 80
 r_pwr = 15
+kick_z_force = 3
+kick_force = 5
+kick_z_force_small = 0
+kick_force_small = 3
 
 
 def on_press(key):
     try:
         #print(type(key))
-        if not (type(key) is keyboard.Key): 
+        if not (type(key) is keyboard.Key):
             if key.char == 'w':
                 cmd.motion_set_point.y = pwr
             if key.char == 's':
@@ -33,17 +37,23 @@ def on_press(key):
                 cmd.motion_set_point.x = -pwr
             if key.char == 'd':
                 cmd.motion_set_point.x = pwr
-        else:
-            if key == keyboard.Key.left: 
+            if key.char == 'q':
                 cmd.motion_set_point.z = r_pwr
-            if key == keyboard.Key.right:
+            if key.char == 'e':
                 cmd.motion_set_point.z = -r_pwr
-            
+            if key.char == 'j':
+                cmd.kicker_set_point.y = kick_z_force_small
+                cmd.kicker_set_point.x = kick_force_small
+            if key.char == 'k':
+                cmd.kicker_set_point.y = kick_z_force
+                cmd.kicker_set_point.x = kick_force
+            if key.char == 'p':
+                cmd.enable_ball_auto_capture = True
+
+
     except AttributeError:
         # print('special key {0} pressed'.format(key))
         pass        
-            
-
 
 def on_release(key):
     if not (type(key) is keyboard.Key):
@@ -55,18 +65,23 @@ def on_release(key):
             cmd.motion_set_point.x = 0
         if key.char == 'd':
             cmd.motion_set_point.x = 0
-    else:
-        if key == keyboard.Key.left: 
+        if key.char == 'q':
             cmd.motion_set_point.z = 0
-        if key == keyboard.Key.right:
+        if key.char == 'e':
             cmd.motion_set_point.z = 0
-
-
+        if key.char == 'j':
+            cmd.kicker_set_point.y = 0
+            cmd.kicker_set_point.x = 0
+        if key.char == 'k':
+            cmd.kicker_set_point.y = 0
+            cmd.kicker_set_point.x = 0
+        if key.char == 'p':
+            cmd.enable_ball_auto_capture = False
 
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
- 
+
 while True:
     print("<", cmd.motion_set_point.x, ", ",
                cmd.motion_set_point.y, ", ",
